8ddf97b1f1ef1e5f434c1cabaf71ef23194ad1cf
yoonjin <yj0331yj@gmail.com>
2024-06-14 22:01:31 +0900

    standard upload

---
+++ dags/dag_bash_operator_standard.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.operators.bash import BashOperator
+
+my_dag = DAG(
+    dag_id="dags_bash_operator_standard",
+    schedule="0 9 * * 1,5"
+    start_date=datetime.datetime(2024, 6, 1),
+    tags=["homework"],
+    catchup=False
+)as dag:
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        dag=my_dag,
+        bash_command="echo whoami",
+    )
+
+    bash_t2 = BashOperator(
+        task_id="bash_t2",
+        dag=my_dag,
+        bash_command="echo $HOSTNAME",
+    )
+
+    bash_t1 >> bash_t2
