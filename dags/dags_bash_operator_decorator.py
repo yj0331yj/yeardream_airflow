cd76d1df164e6d181bf21696a03dc1621ac95079
yoonjin <yj0331yj@gmail.com>
2024-06-16 17:47:28 +0900

    change operators

---
+++ dags/dags_bash_operator_decorator.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.operators.bash import BashOperator
+
+
+@dag(
+    dag_id="dags_bash_operator_decorator",
+    schedule="0 13 * * 5#2",
+    start_date=pendulum.datetime(2024, 5, 1, tz="Asia/Seoul"),
+    tags=["homework"],
+    catchup=False
+)
+def generate_dag():
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="echo whoami",
+    )
+    bash_t2 = BashOperator(
+        task_id="bash_t2",
+        bash_command="echo $HOSTNAME",
+    )
+
+    bash_t1 >> bash_t2
--- dags/dags_bash_operator_standard.py
+++ dags/dags_bash_operator_standard.py
@@ -3,24 +3,20 @@ import datetime
 import pendulum
 from airflow.operators.bash import BashOperator
 
+
 my_dag = DAG(
     dag_id="dags_bash_operator_standard",
-    schedule="0 9 * * 1,5"
-    start_date=datetime.datetime(2024, 6, 1),
+    schedule="0 9 * * 1,5",
+    start_date=pendulum.datetime(2023, 6, 1, tz="Asia/Seoul"),
     tags=["homework"],
     catchup=False
 )
-
 bash_t1 = BashOperator(
     task_id="bash_t1",
-    dag=my_dag,
     bash_command="echo whoami",
 )
-
 bash_t2 = BashOperator(
     task_id="bash_t2",
-    dag=my_dag,
     bash_command="echo $HOSTNAME",
 )
-
 bash_t1 >> bash_t2
