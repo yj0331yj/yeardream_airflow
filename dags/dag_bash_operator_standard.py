c143f08401821b30eace120ea4d45558fe85b61a
yoonjin <yj0331yj@gmail.com>
2024-06-16 14:51:39 +0900

    test

--- dags/dag_bash_operator_standard.py
+++ dags/dag_bash_operator_standard.py
@@ -9,17 +9,18 @@ my_dag = DAG(
     start_date=datetime.datetime(2024, 6, 1),
     tags=["homework"],
     catchup=False
-)as dag:
-    bash_t1 = BashOperator(
-        task_id="bash_t1",
-        dag=my_dag,
-        bash_command="echo whoami",
-    )
+)
 
-    bash_t2 = BashOperator(
-        task_id="bash_t2",
-        dag=my_dag,
-        bash_command="echo $HOSTNAME",
-    )
+bash_t1 = BashOperator(
+    task_id="bash_t1",
+    dag=my_dag,
+    bash_command="echo whoami",
+)
 
-    bash_t1 >> bash_t2
+bash_t2 = BashOperator(
+    task_id="bash_t2",
+    dag=my_dag,
+    bash_command="echo $HOSTNAME",
+)
+
+bash_t1 >> bash_t2
--- dags/test.py
+++ dags/test.py
@@ -1,15 +1,17 @@
 from airflow import DAG
 import datetime
-import pendulum
 from airflow.operators.bash import BashOperator
 
-with DAG(
-    dag_id="dags_bash_operator",
-    schedule="0 0 * * *",
-    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+my_dag = DAG(
+    dag_id="dags_bash_operator_standard",
+    schedule_interval="0 9 * * 1,5",  # schedule_interval로 수정
+    start_date=datetime.datetime(2024, 6, 1),
+    tags=["homework"],
     catchup=False
-) as dag:
-    
+)
+
+# with 블록을 사용하여 DAG context를 설정
+with my_dag:
     bash_t1 = BashOperator(
         task_id="bash_t1",
         bash_command="echo whoami",
@@ -21,4 +23,3 @@ with DAG(
     )
 
     bash_t1 >> bash_t2
-
